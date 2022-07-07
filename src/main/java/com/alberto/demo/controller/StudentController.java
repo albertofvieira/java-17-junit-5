5b0ef5ddeb562be8fab0ebbe8c71ad06f81a26f8
Alberto Vieira <albertofonsecavieira@gmail.com>
2022-07-07 13:07:13 -0300

    coding

---
+++ src/main/java/com/alberto/demo/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.alberto.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
